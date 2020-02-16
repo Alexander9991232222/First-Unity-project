c6146d22cc71ba40702adc8a2291d71a35be5a0f
Alexander <mikhailov.aleksander2018@gmail.com>
2020-02-16 03:09:19 +0200

    add test commit
    commit

--- Assets/Flat Platformer Template/hello.cs
+++ Assets/Flat Platformer Template/hello.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class hello : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
