2e206b17fa72b86927440babff333933c6fbe942
Alexander <mikhailov.aleksander2018@gmail.com>
2020-02-16 03:14:28 +0200

    test commit
    add hello world to test script

--- Assets/Flat Platformer Template/hello.cs
+++ Assets/Flat Platformer Template/hello.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ public class hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.WriteLine("Hello world");
     }
 
     // Update is called once per frame
