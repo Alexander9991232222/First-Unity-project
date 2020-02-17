9f9443a892407627e45f94a6d79bbe0b54e51039
Tabriz <Tabriz1703@gmail.com>
2020-02-17 21:52:19 +0200

    commit

---
+++ Assets/Flat Platformer Template/Scripts/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    public float runSpeed = 3f;
+    void Move(float ax){
+        Vector3 direction = transform.right * ax;
+        transform.position = Vector3.Lerp (transform.position,transform.position+direction,runSpeed*Time.deltaTime);
+    }
+}
---
+++ Assets/Flat Platformer Template/Scripts/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(UserController))]
+public class UserController : MonoBehaviour{
+
+    private UserController pc;
+    // Start is called before the first frame update
+    void Start(){
+        pc = GetComponent<UserController> ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pc.Move (Input.GetAxis("Horizontal"));
+        
+    }
+}
